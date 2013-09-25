a2ac1a38e60d68b31a7242cd3753a0b72b8e77a3
xxValiumxx <takeyourvalium@gmail.com>
2013-09-24 22:30:42 -0700

    Could it be so simple?  fread() certainly cant handle it.

--- application/libraries/WebSocketClient.php
+++ application/libraries/WebSocketClient.php
@@ -30,7 +30,8 @@ class WebsocketClient
 	{
 		// send actual data:
 		fwrite($this->_Socket, "\x00" . $data . "\xff" ) or die('Error:' . $errno . ':' . $errstr); 
-		$wsData = fread($this->_Socket, 32768);
+		//$wsData = fread($this->_Socket, 32768);
+        $wsData = stream_get_contents($this->_Socket, -1);
 		$retData = trim($wsData,"\x00\xff");        
 		return $retData;
 	}
